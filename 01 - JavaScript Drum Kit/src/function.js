99955003ab3b841b7c8b96394a081462cc4bb947
zpoet-coder <zpoet.personal@gmail.com>
2024-07-11 23:15:40 +0800

    complete exercise 01 - Drum Kit

---
+++ 01 - JavaScript Drum Kit/src/function.js
@@ -0,0 +1,18 @@
+export function playSound(event) {
+  // console.log(event);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  if (!audio) return;
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  // console.log('audioElement', audio, '\nkeyElement', key);
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+export function removeClassName(event) {
+  // console.log(event);
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  if (!key) return;
+  key.classList.remove("playing");
+}
